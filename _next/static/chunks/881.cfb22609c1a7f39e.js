86ff260064842ae222c1466673fb9a0c76960503
mathewjordan <mat@northwestern.edu>
2023-12-05 12:40:54 +0000

    Deploying to gh-pages from @ canopy-iiif/example-ia-usgs-ri@0ace92bd7fe8f76d6f35c4ffa070874d918345b5 🚀

---
+++ _next/static/chunks/881.cfb22609c1a7f39e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[881],{8881:function(i){i.exports=JSON.parse('{"collection":"https://iiif.archive.org/iiif/3/usgs_ri/collection.json","metadata":["landmark"]}')}}]);
